825f5a9aa0e9504fef426037a0048716a0928659
SOxWHATxDO <Dimas03novik@mail.ru>
2025-11-05 14:18:00 +0300

    add VRCamera

---
+++ Script/camera_vr.gd
@@ -0,0 +1,20 @@
+extends Node
+@onready var players := {
+	"1": {
+		viewport = $HBoxContainer/SubViewportContainer/SubViewport,
+		camera = $HBoxContainer/SubViewportContainer/SubViewport/Camera2D
+	},
+	"2": {
+		viewport = $HBoxContainer/SubViewportContainer2/SubViewport,
+		camera = $HBoxContainer/SubViewportContainer2/SubViewport/Camera2D
+	}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	players["2"].viewport.world_2d = players["1"].viewport.world_2d
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
